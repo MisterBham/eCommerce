7061edd36f7b927f291bc537ba6e5ce1a63f25c0
MisterBham <brianjhamlin@gmail.com>
2023-11-06 21:28:42 -0600

    setting up server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql2');
+const controllers = require('./src/controllers');
+
+const PORT = process.env.PORT || 3006;
+const DBuser = process.env.DB_USER;
+const DBpassword = process.env.DB_PASSWORD;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(controllers);
+
+if (!DBuser || !DBpassword) {
+  throw new Error("Ensure DBuser and/or DBpassword inputs are correct in .env file. If using the provided .env.EXAMPLE file, be sure to rename file to .env, removing the ending .EXAMPLE")
+}
+
+const db = mysql.createPool(
+  {
+    connectionLimit: 10,
+    queueLimit: 0,
+    waitForConnections: true,
+    host: 'localhost',
+    user: DBuser,
+    password: DBpassword,
+    database: 'ecommerce_db'
+  },
+  console.log(`Connected to the ecommerce_db database.`)
+);
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- src/db/schema.sql
+++ src/db/schema.sql
@@ -3,17 +3,81 @@ CREATE DATABASE ecommerce_db;
 
 USE ecommerce_db;
 
-CREATE TABLE shoes (
-  id INT NOT NULL,
-  name VARCHAR(50) NOT NULL
+CREATE TABLE fashion (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+  -- sku INT NOT NULL
 );
 
-CREATE TABLE t_shirts (
-  id INT NOT NULL,
-  name VARCHAR(50) NOT NULL
+CREATE TABLE home (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
 
-CREATE TABLE button_down (
-  id INT NOT NULL,
-  name VARCHAR(50) NOT NULL
+CREATE TABLE beauty (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE electronics (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE sporting_goods (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE toys (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE automotive (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE pets (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE baby (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE books (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE video_games (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE music (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE office (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
