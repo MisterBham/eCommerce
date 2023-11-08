65ce96f3154187ebd732da7922a46adae2228ed8
MisterBham <brianjhamlin@gmail.com>
2023-11-08 12:01:27 -0600

    working node server

---
+++ src/db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO fashion (name, description)
+VALUES ('Test name', 'Test description'),
+       ('Test name2', 'Test description2'),
+       ('Test name3', 'Test description3'),
+       ('Test name4', 'Test description4');
+       
+INSERT INTO home (name, description)
+VALUES ('Test name', 'Test description'),
+       ('Test name2', 'Test description2'),
+       ('Test name3', 'Test description3'),
+       ('Test name4', 'Test description4');
